6ca117901b3634d16b15e5bb71383e058873f1e1
CHIANG CHENG LUNG <admin@wuchang.life>
2026-06-27 04:19:25 +0000

    Add Wuchang property and cafe menu governance views

--- Taiji_Odoo/addons/wuchang_core/controllers/__init__.py
+++ Taiji_Odoo/addons/wuchang_core/controllers/__init__.py
@@ -4,3 +4,7 @@ from . import sister_controller
 from . import line_webhook_controller
 from . import voice_interface_controller
 from . import xiaoj_ordering_app_controller
+from . import device_app_controller
+from . import device_enrollment_controller
+from . import device_query_controller
+from . import ui_proxy_controller
