6a1658e1f00f8009ee8922e3be529b19f935c3d5
CHIANG CHENG LUNG <admin@wuchang.life>
2026-07-07 19:31:11 +0800

    Feat(Odoo): 實作志工點數四級簽核 UI 視圖常識代碼

--- Taiji_Odoo/addons/wuchang_core/__manifest__.py
+++ Taiji_Odoo/addons/wuchang_core/__manifest__.py
@@ -3,7 +3,7 @@
     'version': '18.0.1.0.0',
     'summary': 'W7TP 五常志工管理與 POS API 核心',
     'depends': ['base'],
-    'data': [],
+    'data': ['views/volunteer_point_views.xml'],
     'installable': True,
     'application': False,
 }
